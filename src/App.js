7f7ab053e413db0bac74820b1c9d3a21955180d2
Alex Preiss <alexspreiss@gmail.com>
2022-02-18 14:27:55 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>mimi poops</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn How to Poop
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
